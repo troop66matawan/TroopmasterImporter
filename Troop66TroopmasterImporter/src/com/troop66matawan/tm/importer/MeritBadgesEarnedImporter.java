6c04ecc1a01cfc24b5c246537ea1490fb58c29af
Troop66 Webmaster <website@troop66matawan.com>
2016-01-11 13:13:58 -0500

    add new importers, due to removal of export from tm

---
+++ Troop66TroopmasterImporter/src/com/troop66matawan/tm/importer/MeritBadgesEarnedImporter.java
@@ -0,0 +1,102 @@
+package com.troop66matawan.tm.importer;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.troop66matawan.tm.model.MeritBadge;
+import com.troop66matawan.tm.model.Scout;
+import com.troop66matawan.tm.model.ScoutFactory;
+
+public class MeritBadgesEarnedImporter extends TroopmasterImporter {
+	private static final int MBNAME_COL1_start = 0;
+	private static final int MBNAME_COL1_end = 19;
+	private static final int MBNAME_COL2_start = 29;
+	private static final int MBNAME_COL2_end = 48;
+	private static final int MBNAME_COL3_start = 58;
+	private static final int MBNAME_COL3_end = 77;
+	private static final int MBDATE_COL1_start = 20;
+	private static final int MBDATE_COL1_end = 27;
+	private static final int MBDATE_COL2_start = 49;
+	private static final int MBDATE_COL2_end = 56;
+	private static final int MBDATE_COL3_start = 78;
+	private static final int MBDATE_COL3_end = 85;
+	
+	private class MBTokenIndex {
+		public int nameStart;
+		public int nameEnd;
+		public int dateStart;
+		public int dateEnd;
+		
+		MBTokenIndex(int nameStartIndex, int nameEndIndex, int dateStartIndex, int dateEndIndex){
+			nameStart = nameStartIndex;
+			nameEnd = nameEndIndex;
+			dateStart = dateStartIndex;
+			dateEnd = dateEndIndex;
+		}
+	}
+	
+	private ArrayList<MBTokenIndex> mbcols = null;
+	
+	public MeritBadgesEarnedImporter(String filename) throws IOException {
+		super(filename);
+		
+		mbcols = new ArrayList<MBTokenIndex>();
+		mbcols.add(new MBTokenIndex(MBNAME_COL1_start, MBNAME_COL1_end, MBDATE_COL1_start, MBDATE_COL1_end));
+		mbcols.add(new MBTokenIndex(MBNAME_COL2_start, MBNAME_COL2_end, MBDATE_COL2_start, MBDATE_COL2_end));
+		mbcols.add(new MBTokenIndex(MBNAME_COL3_start, MBNAME_COL3_end, MBDATE_COL3_start, MBDATE_COL3_end));
+		
+	}
+	
+	private String trimMBName(String line, int startIndex, int endIndex) {
+		String mbName = null;
+		if ( endIndex <= line.trim().length())
+			mbName = line.substring(startIndex, endIndex+1).replace('#', ' ').replace('*', ' ').trim();
+		return mbName;
+	}
+	
+	private void parseMB(Scout s, String line,  MBTokenIndex indexes) {
+		String mbName = trimMBName(line, indexes.nameStart, indexes.nameEnd);
+		if (mbName != null)	{
+			String dateToken = line.substring(indexes.dateStart, indexes.dateEnd+1);
+			MeritBadge mb = new MeritBadge(mbName);
+			mb.set_earned(stringToDate(dateToken));
+			s.addMeritBadge(mb);			
+		}
+		
+	}
+	private void parseMBLine(Scout s, String line) {
+		if (line.length() >= MBDATE_COL1_end) {
+			for (MBTokenIndex indexes : mbcols)	{
+				parseMB(s, line, indexes );
+			}
+		}
+	}
+	public void doImport() throws IOException {
+		ScoutFactory f = ScoutFactory.getInstance();
+		String line = _reader.readLine();
+		Scout scout = null;
+		while (line != null) {
+			// if no current scout, look for line containing string "Rank:"
+			if (scout == null  &&  line.contains("Rank:")) {
+				// Substring before comma is last name, from comma to space is first name
+				int commaIndex = line.indexOf(",");
+				String lName = line.substring(0, commaIndex);
+				int spaceAfterNameIndex = line.indexOf(" ", commaIndex+2);
+				String fName = line.substring(commaIndex+2, spaceAfterNameIndex);
+
+				if (!f.hasScout(lName, fName)) {
+					f.addScout(lName, fName);
+				}
+				scout = f.getScout(lName, fName);
+			} else if (scout != null) {
+				if (line.contains("\f")){
+					scout = null;
+				} else {
+					// Have scout, now get list of MBs
+					parseMBLine(scout, line);					
+				}
+			}
+			line = _reader.readLine();			
+		}
+	}
+}
---
+++ Troop66TroopmasterImporter/src/com/troop66matawan/tm/importer/RankBadgeMatrixImporter.java
@@ -0,0 +1,100 @@
+package com.troop66matawan.tm.importer;
+
+import java.io.IOException;
+import java.util.Date;
+
+import com.troop66matawan.tm.model.RankAdvancement;
+import com.troop66matawan.tm.model.Scout;
+import com.troop66matawan.tm.model.ScoutFactory;
+
+public class RankBadgeMatrixImporter extends TroopmasterImporter {
+	private static final int SCOUT_DATE_startindex = 31;
+	
+	private enum Ranks {
+		SCOUT,
+		TENDERFOOT,
+		SecondClass,
+		FirstClass,
+		Star,
+		Life,
+		Eagle
+	}
+
+	public RankBadgeMatrixImporter(String filename) throws IOException {
+		super(filename);
+	}
+	
+	public void doImport() throws IOException {
+		ScoutFactory f = ScoutFactory.getInstance();
+		String line = _reader.readLine();
+		while (line != null) {
+			if (line.length() > SCOUT_DATE_startindex)
+			{
+				if (line.contains(",")){
+					parseRankDateLine(f, line);
+				}
+					
+			}
+			line = _reader.readLine();
+		}
+	}
+
+	private void parseRankDateLine(ScoutFactory f, String line) {
+		Scout scout = getScout(f, line);
+		RankAdvancement ra = new RankAdvancement();
+		int index = SCOUT_DATE_startindex;
+		for (Ranks r : Ranks.values()) {
+			String dateString = getDateString(line,index);
+			if (dateString.length() > 1) {
+				Date date = stringToDate(dateString);
+				switch (r) {
+				case SCOUT:
+					ra.set_scout(date);
+					break;
+				case TENDERFOOT:
+					ra.set_tenderfoot(date);
+					break;
+				case SecondClass:
+					ra.set_2ndClass(date);
+					break;
+				case FirstClass:
+					ra.set_1stClass(date);
+					break;
+				case Star:
+					ra.set_star(date);
+					break;
+				case Life:
+					ra.set_life(date);
+					break;
+				case Eagle:
+					ra.set_eagle(date);
+					break;
+				}
+			}
+			index += 10;
+		}
+		scout.set_rankAdvancement(ra);
+		f.updateScout(scout);
+	}
+
+	private String getDateString(String line, int startIndex) {
+		int spaceAfterIndex = line.indexOf(" ", startIndex);
+		String dateString = line.substring(startIndex, spaceAfterIndex).trim();
+		
+		return dateString;
+	}
+	
+	private Scout getScout(ScoutFactory f, String line) {
+		// Substring before comma is last name, from comma to space is first name
+		int commaIndex = line.indexOf(",");
+		String lName = line.substring(0, commaIndex);
+		int spaceAfterNameIndex = line.indexOf(" ", commaIndex+2);
+		String fName = line.substring(commaIndex+2, spaceAfterNameIndex);
+
+		if (!f.hasScout(lName, fName)) {
+			f.addScout(lName, fName);
+		}
+		return f.getScout(lName, fName);
+	}
+
+}
